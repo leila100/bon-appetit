2ad377a10d368e454ec5ff4840fdd6e846f023b0
Leila <nb.leila10@gmail.com>
2020-02-22 17:28:15 -0600

    Configure header

--- screens/Categories.js
+++ screens/Categories.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Platform } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
-
-const renderGridItem = itemData => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
+import Colors from "../constants/Colors";
 
 const Categories = props => {
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => props.navigation.navigate({ routeName: "CategoryRecipes" })}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
 };
+
+Categories.navigationOptions = {
+  headerTitle: "Recipe Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "white"
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
+};
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
