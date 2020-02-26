eee19b09e7bdf56144dac6e4a19db132b944aa02
Leila <nb.leila10@gmail.com>
2020-02-26 12:42:14 -0600

    Add text to show no favorites were added

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector } from "react-redux";
 
 import RecipeList from "../components/RecipeList";
 import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 const Favorites = props => {
   const favRecipes = useSelector(state => state.recipes.favoriteRecipes);
 
-  // const favRecipes = favoriteRecipes.filter(recipe => recipe.id === "m1" || recipe.id === "m2");
+  if (!favRecipes || favRecipes.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite recipes found. Start adding some.</DefaultText>
+      </View>
+    );
+  }
+
   return <RecipeList listData={favRecipes} navigation={props.navigation} />;
 };
 
@@ -28,5 +37,13 @@ Favorites.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default Favorites;
